9845508bbb4c730f09224f401496c91e012e4de9
luobaosong <paul_pine@163.com>
2024-02-05 14:12:34 +0800

    init: 项目初始化

---
+++ squirrel-shop-web/src/main/java/org/squirrel/Main.java
@@ -0,0 +1,11 @@
+package org.squirrel;
+
+/**
+ * @author luobaosong
+ * @date 2024-02-01 17:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
