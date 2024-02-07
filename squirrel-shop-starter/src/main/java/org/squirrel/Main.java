ae70a07c166c628115dca8206ca8c2eb42296e12
luobaosong <paul_pine@163.com>
2024-02-07 10:43:20 +0800

    logback: 设置logback日志

--- squirrel-shop-starter/src/main/java/org/squirrel/Main.java
+++ squirrel-shop-starter/src/main/java/org/squirrel/Main.java
@@ -1,5 +1,7 @@
 package org.squirrel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        logger.info("Squirrel started!");
     }
 
 
     @GetMapping("/hello")
     public String hello() {
+        logger.info("hello info!");
+        logger.warn("hello warn!");
+        logger.error("hello error!");
         return "hello squirrel";
     }
 }
